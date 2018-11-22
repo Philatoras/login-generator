8dba22aa820d7552f4f700d4a89e09845e1e975b
Philatoras <lucas.clement@live.fr>
2018-11-22 14:47:34 +0100

    release 1.1.0

--- src/main/java/geco/PasswordGeneration.java
+++ src/main/java/geco/PasswordGeneration.java
@@ -5,10 +5,11 @@ import java.util.Random;
 
 public class PasswordGeneration {
 
-    public String getRandomPassword(){
+    public String getRandomPassword() {
         byte[] array = new byte[8]; // length is bounded by 8
         new Random().nextBytes(array);
         String generatedString = new String(array, Charset.forName("UTF-8"));
-
+        return generatedString;
+    }
 
 }
